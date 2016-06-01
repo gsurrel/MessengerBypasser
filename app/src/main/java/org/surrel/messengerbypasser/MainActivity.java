614f38cdddaf6e7869515d0b20aa587070584bcb
Grégoire Surrel <gregoire.surrel@epfl.ch>
2016-06-01 18:39:41 +0200

    Made activity invisible and removed template layout

--- app/src/main/java/org/surrel/messengerbypasser/MainActivity.java
+++ app/src/main/java/org/surrel/messengerbypasser/MainActivity.java
@@ -8,16 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        Intent i = getIntent();
-//        if(i != null) {
-//            Log.i("Messenger bypasser",""+i);
-//        }
-
-        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(""));
-        //startActivity(intent);
-
         finish();
     }
 }
